146e95884147d56bd9bf3a80ad4ce01d30703dfb
zhuzm <2465241120@qq.com>
2023-09-14 18:04:23 +0800

    添加注释

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -17,10 +17,13 @@ let inputbox = reactive(new Array(4).fill().map((item, index) => ({ id: index, l
 let phoneNum = ref('');
 let isMaxLength = ref(true);
 
+// 四位短信验证码
 const inputCodeValue = computed(() => inputbox.reduce((pre, item) => pre + item.labelValue, ''))
 
+// 验证码的长度
 const focusIndex = computed(() => inputCodeValue.value.length)
 
+// 监听键盘上的删除按键(Backspace)
 const handleListenDelete = (e) => {
     if (e.keyCode === 8 && focusIndex.value > 0) {
         inputbox[focusIndex.value - 1].labelValue = '';
@@ -31,10 +34,12 @@ const handleListenDelete = (e) => {
 
 onMounted(() => {
     // #ifdef H5
+    // 处理聚焦第一个输入框
     document.querySelectorAll('.uni-input-input')[0].focus()
 
     document.addEventListener('keydown', handleListenDelete)
 
+    // 监听输入区域的粘贴事件
     document.querySelector('.code-input').addEventListener('paste', (event) => {
         const pasteText = (event.clipboardData || window.clipboardData).getData("text");
         const arr = pasteText.split('').filter(item => /\d/.test(Number(item)));
@@ -46,7 +51,7 @@ onMounted(() => {
     // #endif
 })
 
-
+// 监听第一个输入框的值：为了处理粘贴和短信自动填充时赋值和聚焦
 watch(() => inputbox[0].labelValue, (val) => {
     if (val) {
         // 处理输入的时候限制输入长度
@@ -68,16 +73,17 @@ watch(() => inputbox[0].labelValue, (val) => {
 
 watch(() => inputCodeValue.value, async (val) => {
     if (!val) {
-        // 处理二次复制粘贴
+        // 处理四位输入框为空时再次复制粘贴
         isMaxLength.value = true;
     }
 
     if (val.length === 4) {
+        // 四位输入框的值已填满 做登录等逻辑操作
         console.log('to login')
     }
 })
 
-
+// 点击输入区域 自动聚焦到空的输入框
 const handleFocus = () => {
     if (focusIndex.value === 4) {
         document.querySelectorAll('.uni-input-input')[3].focus();
@@ -86,6 +92,7 @@ const handleFocus = () => {
     document.querySelectorAll('.uni-input-input')[focusIndex.value].focus();
 }
 
+// 监听输入输入框 自动聚焦到下一个输入框
 const onInput = (e, index) => {
     // index < 3 ，如果是第4格，不触发光标移动至下一个输入框。
     if (inputbox[index].labelValue && index < 3) {
